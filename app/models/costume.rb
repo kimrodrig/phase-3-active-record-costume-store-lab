b019be8264efbb129429866c3f0628fe48ecb346
Kim <rodrigkim@gmail.com>
2022-10-18 19:42:03 -0400

    commit tree f8af12662dc3f1167b01911ad937e181c7432ef5

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,11 @@
-# Create your Costume class here
-# It should inherit from ActiveRecord::Base
+class Costume < ActiveRecord::Migration[6.1]
+    def change
+        create_table :costumes do |t|
+            t.string :name
+            t.float :price
+            t.string :size
+            t.string :image_url
+            t.timestamps null: false
+        end
+    end
+end
